fa32d630777b13cbdf70bda64d7363e24c226481
carlos <carlosbarreraojeda@gmail.com>
2018-03-08 23:16:33 -0600

    make a ejs page

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var app = express();
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+  //static pages will be found in dir public
   app.use(express.static(path.join(__dirname, 'public')))
+  //static pages will be found in views
   app.set('views', path.join(__dirname, 'views'))
   app.set('view engine', 'ejs')
+
   app.get('/', (req, res) => res.render('pages/index'))
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
